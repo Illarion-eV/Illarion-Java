e947d41befd9a8e4e00332dc1f4156914c49fc62
vilarion <vilarion@illarion.org>
2011-08-12 04:13:59 +0200

    add app icon

--- illaeasyquest/src/illarion/easyquest/gui/MainFrame.java
+++ illaeasyquest/src/illarion/easyquest/gui/MainFrame.java
@@ -28,6 +28,9 @@ public class MainFrame extends JRibbonFrame
 	{
 		super("easyQuest Editor");
 		
+		setApplicationIcon(Utils
+            .getResizableIconFromResource("easyquest.png"));
+		
 		final RibbonTask graphTask =
             new RibbonTask(Lang.getMsg(getClass(), "ribbonTaskQuest"),
                 new ClipboardBand(), new GraphBand());
