b3e994599efd79312230d4b82f8a753669b6ab77
martin <martin.polak@gmx.at>
2011-08-28 22:22:32 +0200

    new handlers

---
+++ illaeasyquest/res/template/handler/createPlayerItem.lua
@@ -0,0 +1,5 @@
+-- Give an item to a player -- Einem Charakter einen Gegenstand geben
+
+local ID = INTEGER -- Item-ID -- Item-ID
+local QUALITY = INTEGER -- Quality (101-999) -- Qualität (101-999)
+local AMOUNT = INTEGER -- Amount -- Anzahl
---
+++ illaeasyquest/res/template/handler/sendMessageToPlayer.lua
@@ -0,0 +1,5 @@
+-- Send message to the player -- Nachricht an den Charakter senden
+
+-- PLAYER
+local MESSAGE_DE = TEXT -- German message -- Deutsche Nachricht
+local MESSAGE_EN = TEXT -- English message -- Englische Nachricht
