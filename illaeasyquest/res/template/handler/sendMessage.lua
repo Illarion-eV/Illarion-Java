53efedd9d53e199ce07eb25217db8a3cfada3c16
vilarion <vilarion@illarion.org>
2011-08-28 19:43:47 +0200

    add example handler template

---
+++ illaeasyquest/res/template/handler/sendMessage.lua
@@ -0,0 +1,6 @@
+-- Send message -- Nachricht senden
+
+local MESSAGE_DE = TEXT -- German message -- Deutsche Nachricht
+local MESSAGE_EN = TEXT -- English message -- Englische Nachricht
+local POSITION = POSITION -- Map position -- Position auf der Karte
+local RADIUS = INTEGER -- Range to be within -- Wirkungsreichweite
