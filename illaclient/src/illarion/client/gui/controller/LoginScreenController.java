004f320bb3f424370babadfe4a0d0ea0eabedc65
vilarion <vilarion@illarion.org>
2011-09-07 02:01:26 +0200

    test nifty gui lists

--- illaclient/src/illarion/client/gui/controller/LoginScreenController.java
+++ illaclient/src/illarion/client/gui/controller/LoginScreenController.java
@@ -1,16 +1,41 @@
 package illarion.client.gui.controller;
 
+import illarion.client.LoginDialog;
+import illarion.client.Servers;
+
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.net.UnknownHostException;
+
+import javax.swing.JTable;
+import javax.swing.SwingUtilities;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.log4j.Logger;
+import org.w3c.dom.Document;
+
 import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.controls.ListBox;
+import de.lessvoid.nifty.controls.TextField;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
 
 public class LoginScreenController implements ScreenController {
 
 	private Nifty nifty;
+	private TextField nameTxt;
+	private TextField passwordTxt;
+	private ListBox charList;
 	
     @Override
     public void bind(Nifty nifty, Screen screen) {
     	this.nifty = nifty;
+    	nameTxt = screen.findNiftyControl("nameTxt", TextField.class);
+    	passwordTxt = screen.findNiftyControl("passwordTxt", TextField.class);
+    	charList = nifty.getScreen("charSelect").findNiftyControl("myListBox", ListBox.class);
     }
 
     @Override
@@ -24,7 +49,7 @@ public class LoginScreenController implements ScreenController {
     }
     
     public void login() {
+    	//updateCharacters();
     	nifty.gotoScreen("charSelect");
     }
-
 }
