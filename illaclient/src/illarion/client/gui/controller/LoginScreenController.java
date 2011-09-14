acf6b939e78f2202faf2d82a56e03ba5c8e9e091
martin <martin.polak@gmx.at>
2011-09-14 22:28:44 +0200

    testing gui

--- illaclient/src/illarion/client/gui/controller/LoginScreenController.java
+++ illaclient/src/illarion/client/gui/controller/LoginScreenController.java
@@ -3,6 +3,9 @@ package illarion.client.gui.controller;
 import illarion.client.Login;
 import illarion.common.util.LoadingManager;
 import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.NiftyIdCreator;
+import de.lessvoid.nifty.builder.ControlBuilder;
+import de.lessvoid.nifty.builder.LayerBuilder;
 import de.lessvoid.nifty.controls.Button;
 import de.lessvoid.nifty.controls.CheckBox;
 import de.lessvoid.nifty.controls.Label;
@@ -13,11 +16,22 @@ import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
 import de.lessvoid.nifty.tools.SizeValue;
 import de.lessvoid.nifty.effects.impl.Hint;
+import de.lessvoid.nifty.effects.Effect;
+import de.lessvoid.nifty.effects.EffectEventId;
+import de.lessvoid.nifty.loaderv2.types.PanelType;
+import de.lessvoid.nifty.builder.PanelBuilder;
+import de.lessvoid.nifty.builder.EffectBuilder;
+import de.lessvoid.nifty.builder.ScreenBuilder;
+import de.lessvoid.nifty.builder.TextBuilder;
+import de.lessvoid.nifty.loaderv2.NiftyLoader;
+import de.lessvoid.nifty.loaderv2.types.NiftyType;
+
 
 public final class LoginScreenController implements ScreenController {
 
 	private Nifty nifty;
     private Screen screen;
+    private PanelType panel;
     
 	private TextField nameTxt;
 	private TextField passwordTxt;
@@ -53,9 +67,9 @@ public final class LoginScreenController implements ScreenController {
         passwordTxt.setText(login.getPassword());
         savePassword.setChecked(login.storePassword());
 
-        Element myLabel=screen.findElementByName("testHint");
+       /* Element myLabel=screen.findElementByName("testHint");
         myLabel.setConstraintX(new SizeValue("100px"));
-        myLabel.setConstraintY(new SizeValue("200px"));
+        myLabel.setConstraintY(new SizeValue("200px"));*/
         
         
     }
@@ -85,12 +99,57 @@ public final class LoginScreenController implements ScreenController {
         }
     	nifty.gotoScreen("charSelect");
     }
-    
-    public void displayLabel() {
-    	int myX=nifty.getNiftyMouse().getX();
-    	int myY=nifty.getNiftyMouse().getY();
-    	
-    }
+ /*   
+    public void createLabel() {
+        final String hintControl = "nifty-default-hint";
+        final String hintStyle = "illarion-hint";
+        final String hintText = "dynamical hint";
+        final int hintDelay = 0;
+        final int offsetX = 200;
+        final int offsetY = 300;
+
+        // Create new Layer
+        final String hintLayerId = NiftyIdCreator.generate();
+        final String hintPanelId = hintLayerId + "-hint-panel";
+
+        Element myElement = new LayerBuilder(hintLayerId) {{
+          childLayoutAbsoluteInside();
+          visible(false);
+          control(new ControlBuilder(hintPanelId, hintControl) {{
+            parameter("hintText", hintText);
+            if (hintStyle != null) {
+              style(hintStyle);
+            }
+          }});
+        }}.build(nifty, nifty.getCurrentScreen(), nifty.getCurrentScreen().getRootElement());
+
+        layer(new LayerBuilder("content") {{
+        	backgroundColor("#fff0");
+        	childLayoutVertical();
+        	
+        	onStartScreenEffect(new FadeEffectBuilder() {{
+	        	startColor("#fff0");
+	        	endColor("#ffff");
+	        	length(1000);
+	        	startDelay(0);
+	        	inherit(true);
+	        	post(false);
+        	}});
+        	
+	        panel(new PanelBuilder() {{
+	            childLayoutVertical();
+	            backgroundColor("#8001");
+	            paddingLeft("7px");
+	            paddingRight("7px");
+	            paddingTop("4px");
+	            paddingBottom("4px");
+	            width("105px");
+	            onActiveEffect(new EffectBuilder("hint") {{
+	              effectParameter("color", "#0008");
+	        }});
+	    }});
+        
+    }*/
     
     public void cancelLogin() {
 //        nifty.closePopup(popupLogin.getId());
