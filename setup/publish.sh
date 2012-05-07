1cefb07ccc4f1bd2f6e42f8e5438d14806d7f596
Martin Karing <nitram@illarion.org>
2012-05-07 18:59:45 +0200

    [Build] Added publishing of the building utility to the publish shell script

--- setup/publish.sh
+++ setup/publish.sh
@@ -4,3 +4,4 @@ rsync "${WORKSPACE}"/setup/*.illares /home/nitram/public_html/downloads
 rsync "${WORKSPACE}"/setup/*.jar /home/nitram/public_html/downloads
 rsync "${WORKSPACE}"/setup/*.jar.pack.gz /home/nitram/public_html/downloads
 rsync "${WORKSPACE}"/setup/lib/*.illares /var/www/illarion/media/java
+rsync "${WORKSPACE}"/bin/illabuild.jar /var/www/illarion/media/java
