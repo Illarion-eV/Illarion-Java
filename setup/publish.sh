d87d23140aa1807c51d5b1f487043cb189c64a81
Martin Karing <nitram@illarion.org>
2012-05-07 02:45:28 +0200

    Fixed publishing script

--- setup/publish.sh
+++ setup/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rsync "${WORKSPACE}/setup/*.illares" /home/nitram/public_html/downloads
-rsync "${WORKSPACE}/setup/*.jar" /home/nitram/public_html/downloads
-rsync "${WORKSPACE}/setup/*.jar.pack.gz" /home/nitram/public_html/downloads
-rsync "${WORKSPACE}/setup/lib/*.illares" /var/www/illarion/media/java
+rsync "${WORKSPACE}"/setup/*.illares /home/nitram/public_html/downloads
+rsync "${WORKSPACE}"/setup/*.jar /home/nitram/public_html/downloads
+rsync "${WORKSPACE}"/setup/*.jar.pack.gz /home/nitram/public_html/downloads
+rsync "${WORKSPACE}"/setup/lib/*.illares /var/www/illarion/media/java
