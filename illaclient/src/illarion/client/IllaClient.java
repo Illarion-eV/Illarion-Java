ebd194dafd07d620b85d1a5d3e6f2968c4bc7bd2
Martin Karing <nitram@illarion.org>
2012-05-10 01:01:38 +0200

    [Client] Limited frame rate to 60 and added new flag for map render debugging

--- illaclient/src/illarion/client/IllaClient.java
+++ illaclient/src/illarion/client/IllaClient.java
@@ -204,7 +204,7 @@ public final class IllaClient {
         }
 
         gameContainer.setAlwaysRender(true);
-        //gameContainer.setTargetFrameRate(60);
+        gameContainer.setTargetFrameRate(60);
 
         try {
             gameContainer.start();
