48adaee6ee0a2451d0055bb946a358b90d843892
Martin Karing <nitram@illarion.org>
2012-06-02 13:52:04 +0200

    [All] Improved publishing script

--- setup/publish.sh
+++ setup/publish.sh
@@ -5,3 +5,7 @@ rsync "${WORKSPACE}"/setup/*.jar /home/nitram/public_html/downloads
 rsync "${WORKSPACE}"/setup/*.jar.pack.gz /home/nitram/public_html/downloads
 rsync "${WORKSPACE}"/setup/lib/*.illares /var/www/illarion/media/java
 rsync "${WORKSPACE}"/bin/illabuild.jar /var/www/illarion/media/java
+rsync "${WORKSPACE}"/illadownload/JNLP-INF/*.jnlp /home/nitram/public_html/downloads
+rsync "${WORKSPACE}"/illadownload/jnlp_ico/*.png /home/nitram/public_html/downloads/jnlp_ico
+rsync "${WORKSPACE}"/illadownload/jnlp_ico/*.icns /home/nitram/public_html/downloads/jnlp_ico
+rsync "${WORKSPACE}"/illadownload/jnlp_ico/*.ico /home/nitram/public_html/downloads/jnlp_ico
