1c7e33026ff80c4d09a47a51bd1879613b3cbcd6
Martin Karing <nitram@illarion.org>
2012-06-03 15:09:38 +0200

    [easyQuest] Fixed method name overlapping that occurred with Java 7

--- illaeasyquest/src/illarion/easyquest/gui/CellEditor.java
+++ illaeasyquest/src/illarion/easyquest/gui/CellEditor.java
@@ -1,53 +1,49 @@
 /*
  * This file is part of the Illarion easyQuest Editor.
  *
- * Copyright 2011 - Illarion e.V.
+ * Copyright © 2012 - Illarion e.V.
  *
- * The Illarion easyQuest Editor is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.
- * 
- * The Illarion easyQuest Editor is distributed in the hope that it will be
- * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with
- * the Illarion easyQuest Editor. If not, see <http://www.gnu.org/licenses/>.
+ * The Illarion easyQuest Editor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Illarion easyQuest Editor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the Illarion easyQuest Editor.  If not, see <http://www.gnu.org/licenses/>.
  */
 package illarion.easyquest.gui;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import java.util.EventObject;
-
+import com.mxgraph.model.mxCell;
 import com.mxgraph.swing.mxGraphComponent;
 import com.mxgraph.swing.view.mxICellEditor;
 import com.mxgraph.view.mxCellState;
-import com.mxgraph.model.mxCell;
-
 import illarion.easyquest.quest.Status;
 import illarion.easyquest.quest.Trigger;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.EventObject;
 
 /**
  * To control this editor, use mxGraph.invokesStopCellEditing, mxGraph.
  * enterStopsCellEditing and mxGraph.escapeEnabled.
  */
-public class CellEditor implements mxICellEditor
-{
-	protected mxGraphComponent graphComponent;
+public class CellEditor implements mxICellEditor {
+    protected mxGraphComponent graphComponent;
 
-	protected transient Object editingCell;
-	protected transient EventObject triggerEvent;
-	
-	protected StatusDialog nodeDialog;
-	protected TriggerDialog triggerDialog;
+    protected transient Object editingCell;
+    protected transient EventObject triggerEvent;
 
-	public CellEditor(mxGraphComponent graphComponent)
-	{   
-		this.graphComponent = graphComponent;
+    protected StatusDialog nodeDialog;
+    protected TriggerDialog triggerDialog;
+
+    public CellEditor(mxGraphComponent graphComponent) {
+        this.graphComponent = graphComponent;
 
         nodeDialog = new StatusDialog(MainFrame.getInstance());
         nodeDialog.addOkayListener(new ActionListener() {
@@ -62,7 +58,7 @@ public class CellEditor implements mxICellEditor
                 stopEditing(true);
             }
         });
-        
+
         triggerDialog = new TriggerDialog(MainFrame.getInstance());
         triggerDialog.addOkayListener(new ActionListener() {
             @Override
@@ -76,141 +72,117 @@ public class CellEditor implements mxICellEditor
                 stopEditing(true);
             }
         });
-	}
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see com.mxgraph.swing.view.mxICellEditor#startEditing(java.lang.Object, java.util.EventObject)
-	 */
-	public void startEditing(Object cell, EventObject evt)
-	{
-		if (editingCell != null)
-		{
-			stopEditing(true);
-		}
+    /*
+      * (non-Javadoc)
+      * @see com.mxgraph.swing.view.mxICellEditor#startEditing(java.lang.Object, java.util.EventObject)
+      */
+    public void startEditing(Object cell, EventObject evt) {
+        if (editingCell != null) {
+            stopEditing(true);
+        }
 
-		mxCellState state = graphComponent.getGraph().getView().getState(cell);
+        mxCellState state = graphComponent.getGraph().getView().getState(cell);
 
-		if (state != null)
-		{
-			editingCell = cell;
-			triggerEvent = evt;
-			
-			if (isVertex(cell))
-			{
-			    Status value = (Status)((mxCell)cell).getValue();
-    			nodeDialog.setLocationRelativeTo(MainFrame.getInstance());
-    			nodeDialog.setName(value.getName());
-    			nodeDialog.setStart(value.isStart());
-    			nodeDialog.setHandlers(value.getHandlers());
-    			nodeDialog.setVisible(true);
-    		}
-    		else
-    		{
-    		    Trigger value = (Trigger)((mxCell)cell).getValue();
-    		    triggerDialog.setLocationRelativeTo(MainFrame.getInstance());
-    			triggerDialog.setName(value.getName());
-    			if (value.getType() != null)
-    			{
-    			    triggerDialog.setType(value.getType());
-    			}
-    			triggerDialog.setId(value.getObjectId());
-    			triggerDialog.setParameters(value.getParameters());
-    			triggerDialog.setConditions(value.getConditions());
-    			triggerDialog.setVisible(true);
-    		}
-		}
-	}
+        if (state != null) {
+            editingCell = cell;
+            triggerEvent = evt;
 
-	/*
-	 * (non-Javadoc)
-	 * @see com.mxgraph.swing.view.mxICellEditor#stopEditing(boolean)
-	 */
-	public void stopEditing(boolean cancel)
-	{
-		if (editingCell != null)
-		{
-			Object cell = editingCell;
-			editingCell = null;
-
-			if (!cancel)
-			{
-				EventObject trig = triggerEvent;
-				triggerEvent = null;
-				
-				if (isVertex(cell))
-				{    
-    				Status value = getCurrentNodeValue();
-    				if (value.isStart())
-    				{
-    				    ((mxCell)cell).setStyle("StartStyle");
-    				}
-    				else
-    				{
-    				    ((mxCell)cell).setStyle("");
-    				}
-    				graphComponent.labelChanged(cell, value, trig);
-    			}
-    			else
-    			{
-    			    Trigger value = getCurrentEdgeValue();
-				    graphComponent.labelChanged(cell, value, trig);
-				}
-			}
-
-            if (isVertex(cell))
-            {
-                nodeDialog.setVisible(false);
+            if (isVertex(cell)) {
+                Status value = (Status) ((mxCell) cell).getValue();
+                nodeDialog.setLocationRelativeTo(MainFrame.getInstance());
+                nodeDialog.setName(value.getName());
+                nodeDialog.setStart(value.isStart());
+                nodeDialog.setHandlers(value.getHandlers());
+                nodeDialog.setVisible(true);
+            } else {
+                Trigger value = (Trigger) ((mxCell) cell).getValue();
+                triggerDialog.setLocationRelativeTo(MainFrame.getInstance());
+                triggerDialog.setName(value.getName());
+                if (value.getType() != null) {
+                    triggerDialog.setTriggerType(value.getType());
+                }
+                triggerDialog.setId(value.getObjectId());
+                triggerDialog.setParameters(value.getParameters());
+                triggerDialog.setConditions(value.getConditions());
+                triggerDialog.setVisible(true);
             }
-            else
-            {
+        }
+    }
+
+    /*
+      * (non-Javadoc)
+      * @see com.mxgraph.swing.view.mxICellEditor#stopEditing(boolean)
+      */
+    public void stopEditing(boolean cancel) {
+        if (editingCell != null) {
+            Object cell = editingCell;
+            editingCell = null;
+
+            if (!cancel) {
+                EventObject trig = triggerEvent;
+                triggerEvent = null;
+
+                if (isVertex(cell)) {
+                    Status value = getCurrentNodeValue();
+                    if (value.isStart()) {
+                        ((mxCell) cell).setStyle("StartStyle");
+                    } else {
+                        ((mxCell) cell).setStyle("");
+                    }
+                    graphComponent.labelChanged(cell, value, trig);
+                } else {
+                    Trigger value = getCurrentEdgeValue();
+                    graphComponent.labelChanged(cell, value, trig);
+                }
+            }
+
+            if (isVertex(cell)) {
+                nodeDialog.setVisible(false);
+            } else {
                 triggerDialog.setVisible(false);
             }
-            
-    		graphComponent.requestFocusInWindow();
-		}
-	}
 
-	public Status getCurrentNodeValue()
-	{
-		Status result = new Status();
-	
+            graphComponent.requestFocusInWindow();
+        }
+    }
+
+    public Status getCurrentNodeValue() {
+        Status result = new Status();
+
         result.setName(nodeDialog.getName());
         result.setStart(nodeDialog.isStart());
         result.setHandlers(nodeDialog.getHandlers());
 
-		return result;
-	}
-	
-    public Trigger getCurrentEdgeValue()
-	{
-		Trigger result = new Trigger();
-	
+        return result;
+    }
+
+    public Trigger getCurrentEdgeValue() {
+        Trigger result = new Trigger();
+
         result.setName(triggerDialog.getName());
         result.setObjectId(triggerDialog.getId());
-        result.setType(triggerDialog.getType());
+        result.setType(triggerDialog.getTriggerType());
         result.setParameters(triggerDialog.getParameters());
         result.setConditions(triggerDialog.getConditions());
 
-		return result;
-	}
+        return result;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see com.mxgraph.swing.view.mxICellEditor#getEditingCell()
-	 */
-	public Object getEditingCell()
-	{
-	    // Countering a jgraphx bug: http://forum.jgraph.com/questions/1991/how-to-prevent-custom-cell-editor-from-hiding-edited-cells-label/2016
-	    return null;
-		// return editingCell;
-	}
+    /*
+      * (non-Javadoc)
+      * @see com.mxgraph.swing.view.mxICellEditor#getEditingCell()
+      */
+    public Object getEditingCell() {
+        // Countering a jgraphx bug: http://forum.jgraph.com/questions/1991/how-to-prevent-custom-cell-editor-from-hiding-edited-cells-label/2016
+        return null;
+        // return editingCell;
+    }
 
-    private boolean isVertex(Object cell)
-    {
-        if (cell != null && cell instanceof mxCell)
-        {
-            final mxCell c = (mxCell)cell;
+    private boolean isVertex(Object cell) {
+        if (cell != null && cell instanceof mxCell) {
+            final mxCell c = (mxCell) cell;
             return c.isVertex();
         }
         return false;
