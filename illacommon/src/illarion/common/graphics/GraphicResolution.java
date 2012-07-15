6e1ed603438fbcec65e169754260fd1eaa581f47
Martin Karing <nitram@illarion.org>
2012-07-15 13:57:15 +0200

    [Common] Cleaned source

--- illacommon/src/illarion/common/graphics/GraphicResolution.java
+++ illacommon/src/illarion/common/graphics/GraphicResolution.java
@@ -1,20 +1,20 @@
 /*
- * This file is part of the Illarion Graphics Engine.
+ * This file is part of the Illarion Common Library.
  *
- * Copyright © 2011 - Illarion e.V.
+ * Copyright © 2012 - Illarion e.V.
  *
- * The Illarion Graphics Engine is free software: you can redistribute i and/or
- * modify it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.
- * 
- * The Illarion Graphics Engine is distributed in the hope that it will be
- * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with
- * the Illarion Graphics Interface. If not, see <http://www.gnu.org/licenses/>.
+ * The Illarion Common Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Illarion Common Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the Illarion Common Library.  If not, see <http://www.gnu.org/licenses/>.
  */
 package illarion.common.graphics;
 
@@ -23,7 +23,7 @@ import java.util.regex.Pattern;
 
 /**
  * This class is used to supply and compare possible display resolutions.
- * 
+ *
  * @author Martin Karing &lt;nitram@illarion.org&gt;
  */
 public final class GraphicResolution {
@@ -33,8 +33,7 @@ public final class GraphicResolution {
     private final int bpp;
 
     /**
-     * The string builder used to construct the strings used to make the
-     * resolution human readable.
+     * The string builder used to construct the strings used to make the resolution human readable.
      */
     private final StringBuilder builder;
 
@@ -55,14 +54,14 @@ public final class GraphicResolution {
 
     /**
      * Constructor for a graphic resolution definition.
-     * 
-     * @param newWidth the width of this resolution in pixel
+     *
+     * @param newWidth  the width of this resolution in pixel
      * @param newHeight the height of this resolution in pixel
-     * @param newBpp the bits per point of this resolution
-     * @param refresh the refresh rate of this resolution in Hz
+     * @param newBpp    the bits per point of this resolution
+     * @param refresh   the refresh rate of this resolution in Hz
      */
     public GraphicResolution(final int newWidth, final int newHeight,
-        final int newBpp, final int refresh) {
+                             final int newBpp, final int refresh) {
         height = newHeight;
         width = newWidth;
         bpp = newBpp;
@@ -76,10 +75,10 @@ public final class GraphicResolution {
      * @param definition the text that is parsed to get the values for the resolution
      * @throws IllegalArgumentException in case the string can't be parsed
      */
-    public GraphicResolution(final String definition) {
+    public GraphicResolution(final CharSequence definition) {
         Pattern pattern = Pattern.compile("(\\d+) x (\\d+) x (\\d+) @ (\\d+)Hz");
         Matcher matcher = pattern.matcher(definition);
-        
+
         builder = new StringBuilder();
         if (matcher.matches()) {
             width = Integer.parseInt(matcher.group(1));
@@ -88,7 +87,7 @@ public final class GraphicResolution {
             refreshRate = Integer.parseInt(matcher.group(4));
             return;
         }
-        
+
         pattern = Pattern.compile("(\\d+) x (\\d+) @ (\\d+)Hz");
         matcher = pattern.matcher(definition);
         if (matcher.matches()) {
@@ -98,17 +97,18 @@ public final class GraphicResolution {
             refreshRate = Integer.parseInt(matcher.group(3));
             return;
         }
-        
+
         pattern = Pattern.compile("(\\d+) x (\\d+) x (\\d+)");
         matcher = pattern.matcher(definition);
         if (matcher.matches()) {
             width = Integer.parseInt(matcher.group(1));
             height = Integer.parseInt(matcher.group(2));
-            bpp = Integer.parseInt(matcher.group(3));;
+            bpp = Integer.parseInt(matcher.group(3));
+            ;
             refreshRate = -1;
             return;
         }
-        
+
         pattern = Pattern.compile("(\\d+) x (\\d+)");
         matcher = pattern.matcher(definition);
         if (matcher.matches()) {
@@ -118,41 +118,41 @@ public final class GraphicResolution {
             refreshRate = -1;
             return;
         }
-        
+
         throw new IllegalArgumentException("Can't parse string.");
     }
 
     /**
      * Compare the resolution with another resolution object.
-     * 
+     *
      * @param compRes the object this one is compared to
      * @return <code>true</code> in case all values of the resolutions are equal
      */
     public boolean equals(final GraphicResolution compRes) {
         return ((height == compRes.height) && (width == compRes.width)
-            && (bpp == compRes.bpp) && (refreshRate == compRes.refreshRate));
+                && (bpp == compRes.bpp) && (refreshRate == compRes.refreshRate));
     }
 
     /**
      * Compare the resolution to a set of resolution values.
-     * 
-     * @param compWidth the width to compare with
-     * @param compHeight the height to compare with
-     * @param compBpp the bits per point to compare with
+     *
+     * @param compWidth   the width to compare with
+     * @param compHeight  the height to compare with
+     * @param compBpp     the bits per point to compare with
      * @param compRefresh the fresh rate to compare with
      * @return <code>true</code> in case all values equal with this instance
      */
     public boolean equals(final int compWidth, final int compHeight,
-        final int compBpp, final int compRefresh) {
+                          final int compBpp, final int compRefresh) {
         return ((height == compHeight) && (width == compWidth)
-            && (bpp == compBpp) && (refreshRate == compRefresh));
+                && (bpp == compBpp) && (refreshRate == compRefresh));
     }
 
     /**
      * Compare this resolution to another object. This will result in a return
      * of false, in case the object is not castable to a String or a
      * GraphicResolution object.
-     * 
+     *
      * @param compObj the object this resolution is to be compared with
      * @return <code>true</code> in case both objects are equal
      */
@@ -170,7 +170,7 @@ public final class GraphicResolution {
     /**
      * Compare the resolution with another string. The string is expected to be
      * created with another instance of this class.
-     * 
+     *
      * @param compString the string to compare with
      * @return <code>true</code> if this object and the string represent the
      *         same resolution
@@ -181,7 +181,7 @@ public final class GraphicResolution {
 
     /**
      * Get the bits per point of this resolution.
-     * 
+     *
      * @return the bits per point of this resolution or -1 in case it was not
      *         possible to determine the bits per point.
      */
@@ -191,7 +191,7 @@ public final class GraphicResolution {
 
     /**
      * Get the height of this resolution in pixel.
-     * 
+     *
      * @return the height of this resolution
      */
     public int getHeight() {
@@ -200,7 +200,7 @@ public final class GraphicResolution {
 
     /**
      * Get the refresh reate of this resolution in Hz.
-     * 
+     *
      * @return the refresh rate in Hz
      */
     public int getRefreshRate() {
@@ -209,7 +209,7 @@ public final class GraphicResolution {
 
     /**
      * Get the width of this resolution in pixel.
-     * 
+     *
      * @return the width of this resolution
      */
     public int getWidth() {
@@ -218,7 +218,7 @@ public final class GraphicResolution {
 
     /**
      * Generate a hash code for this graphic resolution object.
-     * 
+     *
      * @return the hashcode of this object
      */
     @Override
@@ -228,7 +228,7 @@ public final class GraphicResolution {
 
     /**
      * Get a human readable string that describes that resolution.
-     * 
+     *
      * @return human readable string to describe that resolution
      */
     @Override
