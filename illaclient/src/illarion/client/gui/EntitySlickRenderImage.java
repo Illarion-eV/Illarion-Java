67321137251e166328692df05945aeeb087695c3
Martin Karing <nitram@illarion.org>
2012-08-07 18:27:36 +0200

    [Client] Added some missing documentation

--- illaclient/src/illarion/client/gui/EntitySlickRenderImage.java
+++ illaclient/src/illarion/client/gui/EntitySlickRenderImage.java
@@ -1,20 +1,20 @@
 /*
  * This file is part of the Illarion Client.
  *
- * Copyright © 2011 - Illarion e.V.
+ * Copyright © 2012 - Illarion e.V.
  *
- * The Illarion Client is free software: you can redistribute i and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- * 
- * The Illarion Client is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU General Public License along with
- * the Illarion Client. If not, see <http://www.gnu.org/licenses/>.
+ * The Illarion Client is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Illarion Client is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the Illarion Client.  If not, see <http://www.gnu.org/licenses/>.
  */
 package illarion.client.gui;
 
@@ -23,17 +23,27 @@ import illarion.client.graphics.AbstractEntity;
 import org.newdawn.slick.Image;
 
 /**
- * This implementation of a slick render image is used to
+ * This implementation of a slick render image is used to show a image that is usually used in the game graphics,
+ * inside the elements of the GUI.
+ *
  * @author Martin Karing &lt;nitram@illarion.org&gt;
  */
 public class EntitySlickRenderImage extends ImageSlickRenderImage {
+    /**
+     * The entity that is supposed to be displayed.
+     */
     private final AbstractEntity entity;
-    
+
+    /**
+     * Create this render image that refers to a specified entity.
+     *
+     * @param entity the entity the image refers to
+     */
     public EntitySlickRenderImage(final AbstractEntity entity) {
         super(null);
         this.entity = entity;
     }
-    
+
     @Override
     protected Image getImage() {
         return entity.getSprite().getTexture(entity.getCurrentFrame());
