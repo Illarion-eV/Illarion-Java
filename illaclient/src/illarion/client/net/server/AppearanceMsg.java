e4b5d85d9c6ccaff9bf12a93149765edd0180ba1
Martin Karing <nitram@illarion.org>
2012-09-23 21:37:04 +0200

    [Client] Fixed apperance message to fit the new format

--- illaclient/src/illarion/client/net/server/AppearanceMsg.java
+++ illaclient/src/illarion/client/net/server/AppearanceMsg.java
@@ -30,8 +30,7 @@ import org.newdawn.slick.Color;
 import java.io.IOException;
 
 /**
- * Servermessage: Character appearance (
- * {@link illarion.client.net.CommandList#MSG_APPEARANCE}).
+ * Servermessage: Character appearance (@link CommandList#MSG_APPEARANCE}).
  *
  * @author Martin Karing &lt;nitram@illarion.org&gt;
  * @author Nop
@@ -212,7 +211,10 @@ public final class AppearanceMsg extends AbstractReply {
     @Override
     public void decode(final NetCommReader reader) throws IOException {
         charId = new CharacterId(reader);
-        appearance = reader.readUShort();
+
+        final int race = reader.readUShort();
+        final boolean male = (int) reader.readByte() == 0x00;
+        appearance = getAppearance(race, male);
         size = reader.readUByte();
         visibilityBonus = reader.readByte();
         hairID = reader.readUByte();
@@ -304,6 +306,219 @@ public final class AppearanceMsg extends AbstractReply {
 
         ch.setVisible(World.getPlayer().canSee(ch));
         return true;
+    }
+
+    /**
+     * Get the appearance for a race and a gender.
+     * TODO: This function is plain and utter crap. It needs to go away. Far away. Soon.
+     *
+     * @param race the race ID
+     * @param male {@code true} in case the character is male
+     * @return the appearance ID
+     */
+    private static int getAppearance(final int race, final boolean male) {
+        switch (race) {
+            case 7:
+                return 3;
+            case 0:
+                return male ? 1 : 16;
+            case 5:
+                return 7;
+            case 1:
+                return male ? 12 : 17;
+            case 4:
+                return male ? 13 : 18;
+            case 3:
+                return male ? 20 : 19;
+            case 2:
+                return male ? 24 : 25;
+            case 9:
+                return 21;
+            case 10:
+                return 2;
+            case 11:
+                return 5;
+            case 12:
+                return 6;
+            case 13:
+                return 8;
+            case 14:
+                return 14;
+            case 15:
+                return 3;
+            case 17:
+                return 4;
+            case 18:
+                return 9;
+            case 19:
+                return 10;
+            case 20:
+                return 11;
+            case 21:
+                return 14;
+            case 22:
+                return 15;
+            case 23:
+                return 22;
+            case 24:
+                return 23;
+            case 25:
+                return 47;
+            case 26:
+                return 27;
+            case 27:
+                return 28;
+            case 28:
+                return 29;
+            case 30:
+                return 31;
+            case 31:
+                return 32;
+            case 32:
+                return 33;
+            case 33:
+                return 34;
+            case 34:
+                return 35;
+            case 37:
+                return 40;
+            case 38:
+                return 36;
+            case 39:
+                return 42;
+            case 40:
+                return 37;
+            case 41:
+                return 38;
+            case 42:
+                return 39;
+            case 43:
+                return 43;
+            case 48:
+                return 46;
+            case 49:
+                return 44;
+            case 50:
+                return 45;
+            case 51:
+                return 51;
+            case 52:
+                return 52;
+            case 53:
+                return 53;
+            case 54:
+                return 54;
+            case 55:
+                return 55;
+            case 56:
+                return 56;
+            case 57:
+                return 57;
+            case 58:
+                return 58;
+            case 59:
+                return 59;
+            case 60:
+                return 60;
+            case 61:
+                return 61;
+            case 62:
+                return 62;
+            case 63:
+                return 63;
+            case 64:
+                return 64;
+            case 65:
+                return 65;
+            case 66:
+                return 66;
+            case 67:
+                return 67;
+            case 68:
+                return 68;
+            case 69:
+                return 69;
+            case 70:
+                return 70;
+            case 71:
+                return 71;
+            case 72:
+                return 72;
+            case 73:
+                return 73;
+            case 74:
+                return 74;
+            case 75:
+                return 75;
+            case 76:
+                return 76;
+            case 77:
+                return 91;
+            case 78:
+                return 77;
+            case 79:
+                return 78;
+            case 80:
+                return 79;
+            case 81:
+                return 80;
+            case 82:
+                return 81;
+            case 83:
+                return 82;
+            case 84:
+                return 83;
+            case 85:
+                return 84;
+            case 86:
+                return 85;
+            case 87:
+                return 86;
+            case 88:
+                return 87;
+            case 89:
+                return 88;
+            case 90:
+                return 89;
+            case 91:
+                return 90;
+            case 92:
+                return 92;
+            case 93:
+                return 93;
+            case 94:
+                return 94;
+            case 95:
+                return 95;
+            case 96:
+                return 96;
+            case 97:
+                return 97;
+            case 98:
+                return 98;
+            case 99:
+                return 99;
+            case 100:
+                return 100;
+            case 101:
+                return 101;
+            case 102:
+                return 102;
+            case 103:
+                return 103;
+            case 104:
+                return 104;
+            case 105:
+                return 105;
+            case 106:
+                return 106;
+            case 107:
+                return 107;
+            case 108:
+                return 108;
+            default:
+                return 1;
+        }
     }
 
     /**
