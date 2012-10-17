c1bfaa7615b1a54761b8447ce56a30ab75f951c6
Martin Karing <nitram@illarion.org>
2012-10-17 21:18:44 +0200

    [easyQuest] Fixed on death trigger

--- illaeasyquest/res/template/trigger/onDeath.lua
+++ illaeasyquest/res/template/trigger/onDeath.lua
@@ -3,34 +3,33 @@ require("monster.base.kills")
 module("onDeath", package.seeall)
 
 -- category: monster
--- Kill a certain amount of monsters -- Eine bestimmte Anzahl an Monstern töten
+-- Kill a certain amount of monsters -- Eine bestimmte Anzahl an Monstern tï¿½ten
 
 local QUEST_NUMBER = 0
 local PRECONDITION_QUESTSTATE = 0
 local POSTCONDITION_QUESTSTATE = 0
 
-local MONSTER_AMNT = INTEGER -- Amount of monsters to be killed -- Anzahl der zu tötenden Monster
+local MONSTER_AMNT = INTEGER -- Amount of monsters to be killed -- Anzahl der zu tï¿½tenden Monster
 
 function onDeath(MONSTER)
-    debug ("*** IN ONDEATH")
-    if monster.base.kills.lastAttacker[MONSTER.id]~=nil then
-        PLAYER = monster.base.kills.lastAttacker[MONSTER.id]  -- get killer
+    if monster.base.kills.hasLastAttacker(MONSTER) then
+        PLAYER = monster.base.kills.getLastAttacker(MONSTER); -- get killer
         if ADDITIONALCONDITIONS(PLAYER)
-        and questsystem.base.fulfilsPrecondition(PLAYER, QUEST_NUMBER, PRECONDITION_QUESTSTATE) then     -- this one is really doing our quest
+                and questsystem.base.fulfilsPrecondition(PLAYER, QUEST_NUMBER, PRECONDITION_QUESTSTATE) then -- this one is really doing our quest
             if killList == nil then
                 killList = {};
             end
             if killList[PLAYER.id] == nil then
                 killList[PLAYER.id] = {};
-                killList[PLAYER.id][MONSTER:getMonsterType()]=0;
+                killList[PLAYER.id][MONSTER:getMonsterType()] = 0;
             end
-            killList[PLAYER.id][MONSTER:getMonsterType()]=killList[PLAYER.id][MONSTER:getMonsterType()]+1;
+            killList[PLAYER.id][MONSTER:getMonsterType()] = killList[PLAYER.id][MONSTER:getMonsterType()] + 1;
             if killList[PLAYER.id][MONSTER:getMonsterType()] == MONSTER_AMNT then
                 HANDLER(PLAYER)
                 killList[PLAYER.id][MONSTER:getMonsterType()] = 0;
-                questsystem.base.setPostcondition(PLAYER, QUEST_NUMBER, POSTCONDITION_QUESTSTATE)
+                questsystem.base.setPostcondition(PLAYER, QUEST_NUMBER, POSTCONDITION_QUESTSTATE);
             end
         end
     end
-    return false
+    return false;
 end
