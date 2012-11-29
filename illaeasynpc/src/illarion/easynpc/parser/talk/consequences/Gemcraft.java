082b699e0bcfffd33a7ecc74a12868c9dfde2618
vilarion <vilarion@illarion.org>
2012-11-29 18:38:37 +0100

    Add new gemcraft consequence

---
+++ illaeasynpc/src/illarion/easynpc/parser/talk/consequences/Gemcraft.java
@@ -0,0 +1,87 @@
+/*
+ * This file is part of the Illarion easyNPC Editor.
+ *
+ * Copyright © 2012 - Illarion e.V.
+ *
+ * The Illarion easyNPC Editor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Illarion easyNPC Editor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the Illarion easyNPC Editor.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package illarion.easynpc.parser.talk.consequences;
+
+import illarion.easynpc.Lang;
+import illarion.easynpc.parsed.talk.TalkConsequence;
+import illarion.easynpc.parsed.talk.consequences.ConsequenceGemcraft;
+import illarion.easynpc.parser.talk.ConsequenceParser;
+import org.fife.ui.rsyntaxtextarea.Token;
+import org.fife.ui.rsyntaxtextarea.TokenMap;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * This is the gemcraft consequence. It is able to parse a gemcraft consequence out of the consequence collection
+ * string.
+ *
+ * @author vilarion &lt;vilarion@illarion.org&gt;
+ */
+public final class Gemcraft extends ConsequenceParser {
+    /**
+     * This pattern is used to find the gemcraft command in the condition and to remove it properly.
+     */
+    @SuppressWarnings("nls")
+    private static final Pattern STRING_FIND = Pattern.compile("\\s*gemcraft\\s*,*\\s*", Pattern.CASE_INSENSITIVE);
+
+    /**
+     * Extract a condition from the working string.
+     */
+    @Override
+    @SuppressWarnings("nls")
+    public TalkConsequence extract() {
+        if (getNewLine() == null) {
+            throw new IllegalStateException("Can't extract if no state set.");
+        }
+
+        final Matcher stringMatcher = STRING_FIND.matcher(getNewLine());
+        if (stringMatcher.find()) {
+            setLine(stringMatcher.replaceFirst(""));
+            return new ConsequenceGemcraft();
+        }
+
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return Lang.getMsg(getClass(), "Docu.description"); //$NON-NLS-1$
+    }
+
+    @Override
+    public String getExample() {
+        return Lang.getMsg(getClass(), "Docu.example"); //$NON-NLS-1$
+    }
+
+    @Override
+    public String getSyntax() {
+        return Lang.getMsg(getClass(), "Docu.syntax"); //$NON-NLS-1$
+    }
+
+    @Override
+    public String getTitle() {
+        return Lang.getMsg(getClass(), "Docu.title"); //$NON-NLS-1$
+    }
+
+    @Override
+    public void enlistHighlightedWords(final TokenMap map) {
+        map.put("gemcraft", Token.RESERVED_WORD);
+    }
+}
