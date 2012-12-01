69c3efcf061792bbcd3c82e0993ae27e6112f10a
Martin Karing <nitram@illarion.org>
2012-12-01 18:32:09 +0100

    [Download] Another try to fix the icons

--- setup/publish.sh
+++ setup/publish.sh
@@ -6,6 +6,6 @@ rsync --update --times "${WORKSPACE}"/setup/*.jar.pack.gz /home/nitram/public_ht
 rsync --update --times "${WORKSPACE}"/setup/lib/*.illares /var/www/illarion/media/java
 rsync --update --times "${WORKSPACE}"/bin/illabuild.jar /var/www/illarion/media/java
 rsync --update --times "${WORKSPACE}"/illadownload/JNLP-INF/*.jnlp /home/nitram/public_html/downloads
-rsync --update --times "${WORKSPACE}"/illadownload/jnlp_ico/*.png /home/nitram/public_html/downloads/jnlp_ico
-rsync --update --times "${WORKSPACE}"/illadownload/jnlp_ico/*.icns /home/nitram/public_html/downloads/jnlp_ico
-rsync --update --times "${WORKSPACE}"/illadownload/jnlp_ico/*.ico /home/nitram/public_html/downloads/jnlp_ico
+rsync --update --times "${WORKSPACE}"/illadownload/jnlp_ico/*.png /home/nitram/public_html/downloads
+rsync --update --times "${WORKSPACE}"/illadownload/jnlp_ico/*.icns /home/nitram/public_html/downloads
+rsync --update --times "${WORKSPACE}"/illadownload/jnlp_ico/*.ico /home/nitram/public_html/downloads
