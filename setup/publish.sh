3a8c6324858431efcf009f0a90ab03f4e716b682
Martin Karing <nitram@illarion.org>
2012-12-01 16:20:11 +0100

    [Build] Improved publish script
    The publish script now handles the timestamps of the files properly and
    performs only updates if neccessary

--- setup/publish.sh
+++ setup/publish.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-rsync "${WORKSPACE}"/setup/*.illares /home/nitram/public_html/downloads
-rsync "${WORKSPACE}"/setup/*.jar /home/nitram/public_html/downloads
-rsync "${WORKSPACE}"/setup/*.jar.pack.gz /home/nitram/public_html/downloads
-rsync "${WORKSPACE}"/setup/lib/*.illares /var/www/illarion/media/java
-rsync "${WORKSPACE}"/bin/illabuild.jar /var/www/illarion/media/java
-rsync "${WORKSPACE}"/illadownload/JNLP-INF/*.jnlp /home/nitram/public_html/downloads
-rsync "${WORKSPACE}"/illadownload/jnlp_ico/*.png /home/nitram/public_html/downloads/jnlp_ico
-rsync "${WORKSPACE}"/illadownload/jnlp_ico/*.icns /home/nitram/public_html/downloads/jnlp_ico
-rsync "${WORKSPACE}"/illadownload/jnlp_ico/*.ico /home/nitram/public_html/downloads/jnlp_ico
+rsync --update --times "${WORKSPACE}"/setup/*.illares /home/nitram/public_html/downloads
+rsync --update --times "${WORKSPACE}"/setup/*.jar /home/nitram/public_html/downloads
+rsync --update --times "${WORKSPACE}"/setup/*.jar.pack.gz /home/nitram/public_html/downloads
+rsync --update --times "${WORKSPACE}"/setup/lib/*.illares /var/www/illarion/media/java
+rsync --update --times "${WORKSPACE}"/bin/illabuild.jar /var/www/illarion/media/java
+rsync --update --times "${WORKSPACE}"/illadownload/JNLP-INF/*.jnlp /home/nitram/public_html/downloads
+rsync --update --times "${WORKSPACE}"/illadownload/jnlp_ico/*.png /home/nitram/public_html/downloads/jnlp_ico
+rsync --update --times "${WORKSPACE}"/illadownload/jnlp_ico/*.icns /home/nitram/public_html/downloads/jnlp_ico
+rsync --update --times "${WORKSPACE}"/illadownload/jnlp_ico/*.ico /home/nitram/public_html/downloads/jnlp_ico
