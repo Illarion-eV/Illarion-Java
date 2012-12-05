a56e4ef369ffe132eb9d89fb00e13bd827c19e5a
vilarion <vilarion@illarion.org>
2012-12-06 00:02:08 +0100

    Add rank condition

---
+++ illaeasynpc/src/illarion/easynpc/parsed/talk/conditions/ConditionRank.java
@@ -0,0 +1,97 @@
+/*
+ * This file is part of the Illarion easyNPC Editor.
+ *
+ * Copyright © 2012 - Illarion e.V.
+ *
+ * The Illarion easyNPC Editor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Illarion easyNPC Editor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the Illarion easyNPC Editor.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package illarion.easynpc.parsed.talk.conditions;
+
+import illarion.easynpc.data.CompareOperators;
+import illarion.easynpc.parsed.talk.AdvancedNumber;
+import illarion.easynpc.parsed.talk.TalkCondition;
+import illarion.easynpc.writer.LuaWriter;
+
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * This class is used to store all required values for the rank condition.
+ *
+ * @author vilarion &lt;vilarion@illarion.org&gt;
+ */
+public final class ConditionRank implements TalkCondition {
+    /**
+     * The code needed for this condition in the easyNPC script.
+     */
+    @SuppressWarnings("nls")
+    private static final String EASY_CODE = "rank %1$s %2$s";
+
+    /**
+     * The LUA code needed for this condition to work.
+     */
+    @SuppressWarnings("nls")
+    private static final String LUA_CODE = "talkEntry:addCondition(%1$s.rank(\"%2$s\", %3$s));" + LuaWriter.NL;
+
+    /**
+     * The LUA module required for this condition to work.
+     */
+    @SuppressWarnings("nls")
+    private static final String LUA_MODULE = BASE_LUA_MODULE + "rank";
+
+    /**
+     * The operator that is used to compare with the player rank.
+     */
+    private final CompareOperators operator;
+
+    /**
+     * The value used to compare the player rank against.
+     */
+    private final AdvancedNumber value;
+
+    /**
+     * The constructor that takes the values for the rank test.
+     *
+     * @param op       the operator that is used to compare the player coins with
+     * @param newValue the value the player rank is compared against
+     */
+    public ConditionRank(final CompareOperators op, final AdvancedNumber newValue) {
+        operator = op;
+        value = newValue;
+    }
+
+    /**
+     * Get the LUA module needed for this condition.
+     */
+    @Override
+    public String getLuaModule() {
+        return LUA_MODULE;
+    }
+
+    /**
+     * Write the easyNPC version of this rank condition.
+     */
+    @Override
+    public void writeEasyNpc(final Writer target) throws IOException {
+        target.write(String.format(EASY_CODE, operator.getLuaComp(), value.getEasyNPC()));
+    }
+
+    /**
+     * Write the LUA code needed for this rank condition.
+     */
+    @Override
+    public void writeLua(final Writer target) throws IOException {
+        target.write(String.format(LUA_CODE, LUA_MODULE, operator.getLuaComp(), value.getLua()));
+    }
+}
