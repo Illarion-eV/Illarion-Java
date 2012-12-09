81f395f3a85c5a1adc2314b054bcda1b4577d06e
Martin Karing <nitram@illarion.org>
2012-12-09 17:25:18 +0100

    [Client] Cleanup book message

--- illaclient/src/illarion/client/net/server/BookMsg.java
+++ illaclient/src/illarion/client/net/server/BookMsg.java
@@ -27,55 +27,34 @@ import org.bushe.swing.event.EventBus;
 import java.io.IOException;
 
 /**
- * Servermessage: Book ( {@link illarion.client.net.CommandList#MSG_BOOK}).
+ * Servermessage: Book ({@link CommandList#MSG_BOOK}).
  *
- * @author Blay09
+ * @author Martin Karing &lt;nitram@illarion.org&gt;
  */
 @ReplyMessage(replyId = CommandList.MSG_BOOK)
 public final class BookMsg extends AbstractReply {
-
     /**
      * The book id that was sent.
      */
     private int bookId;
 
-    /**
-     * Decode the book data the receiver got and prepare it for the execution.
-     *
-     * @param reader the receiver that got the data from the server that needs
-     *               to be decoded
-     * @throws IOException thrown in case there was not enough data received to
-     *                     decode the full message
-     */
     @Override
     public void decode(final NetCommReader reader) throws IOException {
         bookId = reader.readUShort();
     }
 
-    /**
-     * Execute the book message and send the decoded data to the rest of the
-     * client.
-     *
-     * @return true if the execution is done, false if it shall be called again
-     */
     @Override
     public boolean executeUpdate() {
         EventBus.publish(new ShowBookEvent(bookId));
         return true;
     }
 
-    /**
-     * Get the data of this book message as string.
-     *
-     * @return the string that contains the values that were decoded for this
-     *         message
-     */
     @SuppressWarnings("nls")
     @Override
     public String toString() {
         final StringBuilder builder = new StringBuilder();
         builder.append("book ");
         builder.append(bookId);
-        return toString(builder.toString());
+        return toString("");
     }
 }
