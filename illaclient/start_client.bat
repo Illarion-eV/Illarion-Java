46d41eed9c07cea538159e4b188ffa415b089c84
vilarion <vilarion@illarion.org>
2012-12-26 05:12:15 +0100

    Fix code-signing
    As per http://docs.oracle.com/javase/tutorial/deployment/doingMoreWithRIA/settingArgsProperties.html
    custom properties in jnlp files are generally considered insecure. Only
    properties prefixed with either "jnlp." or "jawaws." are considered secure
    and will not break code-signing. The only correctly signed jnlp so far was
    the loader's release jnlp since it does not contain any property. Also it
    should be safe to remove jnlp-signing now.

--- illaclient/start_client.bat
+++ illaclient/start_client.bat
@@ -1,4 +1,4 @@
 @echo off
 
 
-java -Dillarion.download.launch=tsclient -Dillarion.download.dev.dir=..\bin -Dillarion.download.devres.dir=..\illacommon\rsc -Dillarion.download.lib.dir=..\illacommon\lib\signed -Dillarion.download.nodownload=true -jar ..\bin\illarion_download.jar
+java -Djnlp.illarion.download.launch=tsclient -Dillarion.download.dev.dir=..\bin -Dillarion.download.devres.dir=..\illacommon\rsc -Dillarion.download.lib.dir=..\illacommon\lib\signed -Dillarion.download.nodownload=true -jar ..\bin\illarion_download.jar
--- illaeasynpc/start.bat
+++ illaeasynpc/start.bat
@@ -1,1 +1,1 @@
-java -Dillarion.download.launch=easynpc -Dillarion.download.dev.dir=..\bin -Dillarion.download.devres.dir=..\illacommon\rsc -Dillarion.download.lib.dir=..\illacommon\lib\signed -Dillarion.download.nodownload=true -jar ..\bin\illarion_download.jar
+java -Djnlp.illarion.download.launch=easynpc -Dillarion.download.dev.dir=..\bin -Dillarion.download.devres.dir=..\illacommon\rsc -Dillarion.download.lib.dir=..\illacommon\lib\signed -Dillarion.download.nodownload=true -jar ..\bin\illarion_download.jar
