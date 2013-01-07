ffcaeec68ae5212912ed87e43264c78f3d51adfb
Martin Karing <nitram@illarion.org>
2013-01-07 18:54:22 +0100

    [IDE] Added additional possibilities for compile time validation

---
+++ illacommon/src/illarion/common/annotation/Nullable.java
@@ -0,0 +1,32 @@
+/*
+ * This file is part of the Illarion Common Library.
+ *
+ * Copyright © 2013 - Illarion e.V.
+ *
+ * The Illarion Common Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Illarion Common Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the Illarion Common Library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package illarion.common.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * This annotation is used to flag things that can be set to {@code null}.
+ *
+ * @author Martin Karing &lt;nitram@illarion.org&gt;
+ */
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
+@Documented
+@Retention(RetentionPolicy.CLASS)
+public @interface Nullable {
+}
