3cf0d5cc5f009619f87245d515a287349c99445c
Martin Karing <nitram@illarion.org>
2013-09-04 11:27:05 +0200

    [Client] Delay execution of the quest available message
    The message is now delayed until the GUI is ready and loaded

--- illaclient/src/illarion/client/net/server/QuestAvailabilityMsg.java
+++ illaclient/src/illarion/client/net/server/QuestAvailabilityMsg.java
@@ -69,6 +69,11 @@ public class QuestAvailabilityMsg extends AbstractReply {
     }
 
     @Override
+    public boolean processNow() {
+        return World.getGameGui().isReady();
+    }
+
+    @Override
     public boolean executeUpdate() {
         final Collection<Location> available = (availableQuests == null) ? Collections.<Location>emptyList() : Arrays.asList(availableQuests);
         final Collection<Location> availableSoon = (availableSoonQuests == null) ? Collections.<Location>emptyList() : Arrays.asList(availableSoonQuests);
