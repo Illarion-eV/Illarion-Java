8e5a19afbe3978feba2ff111a150d81743b6541c
Martin Karing <nitram@illarion.org>
2013-11-15 21:07:19 +0100

    [Downloader] Updated style of data directory selection

--- illadownload/src/main/java/illarion/download/gui/GuiApplication.java
+++ illadownload/src/main/java/illarion/download/gui/GuiApplication.java
@@ -4,24 +4,43 @@ import illarion.download.gui.model.GuiModel;
 import illarion.download.gui.view.DataDirSelectView;
 import illarion.download.gui.view.MainView;
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 /**
  * @author Martin Karing &lt;nitram@illarion.org&gt;
  */
 public class GuiApplication extends Application {
+    private static final double SCENE_WIDTH = 600.0;
+    private static final double SCENE_HEIGHT = 400.0;
+
+    @Nullable
+    private Stage stage;
+
     @Override
     public void start(Stage stage) throws Exception {
         stage.initStyle(StageStyle.TRANSPARENT);
         final GuiModel model = new GuiModel(stage, getHostServices());
 
-        stage.setScene(new Scene(new MainView(model), 600, 400));
-        stage.getScene().setFill(Color.TRANSPARENT);
-        stage.show();
+        this.stage = stage;
 
+        setScene(new DataDirSelectView(model));
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public void setScene(@Nonnull final Parent sceneContent) {
+        if (stage == null) {
+            return;
+        }
+        stage.setScene(new Scene(sceneContent, SCENE_WIDTH, SCENE_HEIGHT));
+        stage.getScene().setFill(null);
     }
 
     public static void main(final String[] args) {
