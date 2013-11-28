c64aa8bfa44afa22c667242c8fcda1d49dfdb98c
vilarion <vilarion@illarion.org>
2013-11-28 01:38:17 +0100

    [Downloader] Add update channel selection window

---
+++ illadownload/src/main/java/illarion/download/gui/controller/ChannelSelectionController.java
@@ -0,0 +1,13 @@
+package illarion.download.gui.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author Andreas Grob &lt;vilarion@illarion.org&gt;
+ */
+public class ChannelSelectionController extends AbstractController {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+}
