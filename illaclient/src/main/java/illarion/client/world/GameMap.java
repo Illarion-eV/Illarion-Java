6ecfdea8ceb0a6d7a6abc5ff70dd7c94ba671ede
Martin Karing <nitram@illarion.org>
2015-01-20 23:01:45 +0100

    [Client] Properly handle mini map resources during logout/login cycle

--- illaclient/src/main/java/illarion/client/world/GameMap.java
+++ illaclient/src/main/java/illarion/client/world/GameMap.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of the Illarion project.
  *
- * Copyright © 2014 - Illarion e.V.
+ * Copyright © 2015 - Illarion e.V.
  *
  * Illarion is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -756,6 +756,7 @@ public final class GameMap implements LightingMap, Stoppable {
     @Override
     public void saveShutdown() {
         clear();
+        miniMap.saveShutdown();
     }
 
     /**
