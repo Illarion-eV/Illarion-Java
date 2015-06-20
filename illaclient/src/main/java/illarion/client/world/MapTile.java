42da798c91da5a9900d9bd67fc9e6deccd88645c
Martin Karing <nitram@illarion.org>
2015-06-20 11:06:54 +0200

    [Client] Improved levels of log messages in map tiles

--- illaclient/src/main/java/illarion/client/world/MapTile.java
+++ illaclient/src/main/java/illarion/client/world/MapTile.java
@@ -168,6 +168,11 @@ public final class MapTile {
     @Nullable
     private MapGroup group;
 
+    /**
+     * Once this value is set {@code true} the tile is assumed to be removed.
+     */
+    private boolean removedTile;
+
     public int getQuestMarkerElevation() {
         return questMarkerElevation;
     }
@@ -261,7 +266,7 @@ public final class MapTile {
     @Nullable
     public Item getTopItem() {
         if (removedTile) {
-            LOGGER.warn("Requested top item of a removed tile.");
+            LOGGER.debug("Requested top item of a removed tile.");
             return null;
         }
 
@@ -275,11 +280,6 @@ public final class MapTile {
     public Item getItem(int index) {
         return items.get(index);
     }
-
-    /**
-     * Once this value is set {@code true} the tile is assumed to be removed.
-     */
-    private boolean removedTile;
 
     /**
      * Remove the light source of this tile in case there is any.
@@ -536,7 +536,7 @@ public final class MapTile {
      */
     public boolean canMoveItem() {
         if (removedTile) {
-            LOGGER.warn("Checking a removed tile for a movable item.");
+            LOGGER.debug("Checking a removed tile for a movable item.");
             return false;
         }
         if (!World.getPlayer().getLocation().isNeighbour(tileCoordinate)) {
@@ -554,7 +554,7 @@ public final class MapTile {
      */
     public int getCoverage() {
         if (removedTile) {
-            LOGGER.warn("Checking the coverage of a removed tile");
+            LOGGER.debug("Checking the coverage of a removed tile");
             return 0;
         }
         if (losDirty) {
@@ -580,7 +580,8 @@ public final class MapTile {
      */
     public int getElevation() {
         if (removedTile) {
-            LOGGER.warn("Checking the elevation of a removed tile.");
+            LOGGER.debug("Checking the elevation of a removed tile.");
+            return 0;
         }
         return items.getElevation();
     }
@@ -592,7 +593,7 @@ public final class MapTile {
      */
     public int getFace() {
         if (removedTile) {
-            LOGGER.warn("Checking the facing flag of a removed tile.");
+            LOGGER.debug("Checking the facing flag of a removed tile.");
             return 0;
         }
         // empty tile accept all light
@@ -655,7 +656,7 @@ public final class MapTile {
      */
     public int getTileMusic() {
         if (removedTile) {
-            LOGGER.warn("Requested the music ID of a removed tile.");
+            LOGGER.debug("Requested the music ID of a removed tile.");
             return 0;
         }
         return musicId;
@@ -677,9 +678,6 @@ public final class MapTile {
      */
     @Nonnull
     public ServerCoordinate getCoordinates() {
-        if (removedTile) {
-            LOGGER.warn("Requesting the coordinates of a removed tile");
-        }
         return tileCoordinate;
     }
 
@@ -690,7 +688,7 @@ public final class MapTile {
      */
     public boolean isBlocked() {
         if (removedTile) {
-            LOGGER.warn("Checking a removed tile if its blocked.");
+            LOGGER.debug("Checking a removed tile if its blocked.");
             return true;
         }
         if (isObstacle()) {
@@ -708,7 +706,7 @@ public final class MapTile {
      */
     public boolean isObstacle() {
         if (removedTile) {
-            LOGGER.warn("Checking a removed tile if its a obstacle.");
+            LOGGER.debug("Checking a removed tile if its a obstacle.");
             return true;
         }
         return getMovementCost() == Integer.MAX_VALUE;
@@ -721,7 +719,8 @@ public final class MapTile {
      */
     public boolean isOpaque() {
         if (removedTile) {
-            LOGGER.warn("Checking opaque value of a removed tile.");
+            LOGGER.debug("Checking opaque value of a removed tile.");
+            return true;
         }
         Tile localTile = tile;
         if (tile == null) {
@@ -741,7 +740,7 @@ public final class MapTile {
      */
     public void renderLight() {
         if (removedTile) {
-            LOGGER.warn("Render light of a removed tile.");
+            LOGGER.debug("Render light of a removed tile.");
             return;
         }
         tracerColor.setColor(tmpLight);
@@ -775,7 +774,7 @@ public final class MapTile {
      */
     public boolean update(@Nonnull TileUpdate update) {
         if (removedTile) {
-            LOGGER.warn("Process update of a removed tile.");
+            LOGGER.error("Process update of a removed tile.");
             return false;
         }
 
@@ -817,7 +816,7 @@ public final class MapTile {
      */
     public void setTileId(int id) {
         if (removedTile) {
-            LOGGER.warn("Change the ID of a removed tile.");
+            LOGGER.error("Change the ID of a removed tile.");
             return;
         }
         losDirty = true;
@@ -857,7 +856,7 @@ public final class MapTile {
      */
     public void resetLight() {
         if (removedTile) {
-            LOGGER.warn("Resetting the light of a removed tile.");
+            LOGGER.debug("Resetting the light of a removed tile.");
         }
         tmpLight.setColor(Color.BLACK);
     }
@@ -944,7 +943,7 @@ public final class MapTile {
     public void updateItems(
             int itemNumber, @Nonnull List<ItemId> itemId, @Nonnull List<ItemCount> itemCount) {
         if (removedTile) {
-            LOGGER.warn("Update items of a removed tile requested.");
+            LOGGER.error("Update items of a removed tile requested.");
             return;
         }
         updateItemList(itemNumber, itemId, itemCount);
