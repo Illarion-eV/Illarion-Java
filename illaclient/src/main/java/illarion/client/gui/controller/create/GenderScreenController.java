40e4a317260eca2b5813d49ca97f8e63cd536b4d
Martin Karing <nitram@illarion.org>
2016-01-17 18:12:21 +0100

    [Client] Added basic screens for character creation

---
+++ illaclient/src/main/java/illarion/client/gui/controller/create/GenderScreenController.java
@@ -0,0 +1,27 @@
+package illarion.client.gui.controller.create;
+
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
+
+import javax.annotation.Nonnull;
+
+/**
+ * @author Martin Karing &lt;nitram@illarion.org&gt;
+ */
+public final class GenderScreenController implements ScreenController {
+    @Override
+    public void bind(@Nonnull Nifty nifty, @Nonnull Screen screen) {
+
+    }
+
+    @Override
+    public void onStartScreen() {
+
+    }
+
+    @Override
+    public void onEndScreen() {
+
+    }
+}
