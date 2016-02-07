f07f024df3eca1a18a1864a6ad2466e5689c45c2
Martin Karing <nitram@illarion.org>
2016-02-07 14:30:42 +0100

    [Client] Using distance for NPCs fixed

--- illaclient/src/main/java/illarion/client/graphics/Avatar.java
+++ illaclient/src/main/java/illarion/client/graphics/Avatar.java
@@ -150,7 +150,7 @@ public final class Avatar extends AbstractEntity<AvatarTemplate> implements Reso
         }
         this.parentChar = parentChar;
 
-        if (parentChar.isHuman()) {
+        if (parentChar.isHuman() || parentChar.isNPC()) {
             int interval = IllaClient.getCfg().getInteger("doubleClickInterval");
             delayedWalkingHandler = new AbstractMultiActionHelper(interval, 2) {
                 @Override
@@ -534,7 +534,7 @@ public final class Avatar extends AbstractEntity<AvatarTemplate> implements Reso
                 if (target != null) {
                     log.debug("Walking to and using the character {}", interactiveChar);
                     TargetMovementHandler handler = World.getPlayer().getMovementHandler().getTargetMovementHandler();
-                    handler.walkTo(parentChar.getLocation(), 1);
+                    handler.walkTo(parentChar.getLocation(), interactiveChar.getUseRange());
                     handler.setTargetReachedAction(interactiveChar::use);
                     handler.assumeControl();
                 } else {
