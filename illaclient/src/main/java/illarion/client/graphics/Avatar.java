Illarion-eV/Illarion-Java

commit 23e94eb057f6abaa442b287e1d0980295ca26f3a

Initial commit of the new LookAtChar system. Future work needs to reduce code redundancy with DialogInput system.
Cherry-Pick: ff5c717

illaclient/src/main/java/illarion/client/graphics/Avatar.java
@@ -21,6 +21,7 @@ import illarion.client.input.AbstractMouseLocationEvent;
 import illarion.client.input.ClickOnMapEvent;
 import illarion.client.input.CurrentMouseLocationEvent;
 import illarion.client.input.DoubleClickOnMapEvent;
+import illarion.client.net.client.LookAtCharCmd;
 import illarion.client.resources.CharacterFactory;
 import illarion.client.resources.MiscImageFactory;
 import illarion.client.resources.Resource;
@@ -124,6 +125,8 @@ public final class Avatar extends AbstractEntity<AvatarTemplate> implements Reso
     private Color targetLight;
 
     private int showHighlight;
+
+    private Input input;
 
     private Avatar(@Nonnull AvatarTemplate template, @Nonnull Char parentChar) {
         super(template);
@@ -307,6 +310,12 @@ public final class Avatar extends AbstractEntity<AvatarTemplate> implements Reso
         return super.isEventProcessed(container, delta, event);
     }
 
+    @Override
+    public void update(@Nonnull GameContainer container, int delta) {
+        input = container.getEngine().getInput();
+        super.update(container, delta);
+    }
+
     /**
      * This function handles click events on the avatars.
      *
@@ -378,8 +387,8 @@ public final class Avatar extends AbstractEntity<AvatarTemplate> implements Reso
         }
 
         if (parentChar.isHuman()) {
-            Char charToName = parentChar;
-            World.getUpdateTaskManager().addTaskForLater((container1, delta1) -> World.getGameGui().getDialogInputGui().showNamingDialog(charToName));
+            //Sending a LookAtCharCmd will open the character window on server response.
+            World.getNet().sendCommand(new LookAtCharCmd(parentChar.getCharId(), LookAtCharCmd.LOOKAT_STARE));
         } else {
             InteractiveChar interactiveChar = parentChar.getInteractive();
 

illaclient/src/main/java/illarion/client/gui/DialogInputGui.java
@@ -15,7 +15,9 @@
  */
 package illarion.client.gui;
 
+import de.lessvoid.nifty.Nifty;
 import illarion.client.world.Char;
+import illarion.common.types.CharacterId;
 
 import javax.annotation.Nonnull;
 
@@ -36,5 +38,6 @@ public interface DialogInputGui {
      */
     void showInputDialog(int dialogId, String title, @Nonnull String message, int maxLength, boolean multiLine);
 
-    void showNamingDialog(@Nonnull Char chara);
+    void showCharacterDialog(@Nonnull CharacterId charId, @Nonnull String lookAt);
+
 }

illaclient/src/main/java/illarion/client/gui/controller/game/DialogHandler.java
@@ -52,6 +52,8 @@ import org.illarion.engine.input.Input;
 import org.illarion.engine.input.Key;
 import org.illarion.nifty.controls.*;
 import org.illarion.nifty.controls.DialogInput.DialogButton;
+import org.illarion.nifty.controls.dialog.input.DialogCharacterControl;
+import org.illarion.nifty.controls.dialog.input.builder.DialogCharacterBuilder;
 import org.illarion.nifty.controls.dialog.input.builder.DialogInputBuilder;
 import org.illarion.nifty.controls.dialog.message.builder.DialogMessageBuilder;
 import org.illarion.nifty.controls.dialog.select.builder.DialogSelectBuilder;
@@ -589,27 +591,32 @@ public final class DialogHandler
     }
 
     @Override
-    public void showNamingDialog(@Nonnull Char chara) {
+    public void showCharacterDialog(@Nonnull CharacterId charId, String lookAt) {
         Element parentArea = screen.findElementById("windows");
-        CharacterId charId = chara.getCharId();
-        if ((charId == null) || !charId.isHuman() || World.getPlayer().isPlayer(charId)) {
+        Char chara = World.getPeople().getCharacter(charId);
+
+        if (chara == null || !chara.isHuman() || World.getPlayer().isPlayer(charId)) {
+            log.warn("Tried to open a character dialog on an invalid character.");
             return;
         }
         String currentCustomName = chara.getCustomName();
-        String dialogName = "namingDialog" + Long.toString(charId.getValue());
+        String dialogName = "characterDialog" + Long.toString(charId.getValue());
         if (parentArea.findElementById(dialogName) != null) {
             return;
         }
-        DialogInputBuilder builder = new DialogInputBuilder(dialogName, Lang.getMsg("gui.dialog.naming.title"));
-        builder.description(String.format(Lang.getMsg("gui.dialog.naming.description"), chara.getName()));
-        builder.buttonLeft(Lang.getMsg("gui.dialog.naming.ok"));
-        builder.buttonRight(Lang.getMsg("gui.dialog.naming.cancel"));
+        DialogCharacterBuilder builder = new DialogCharacterBuilder(dialogName, Lang.getMsg("gui.dialog.character.title"));
+        builder.description(String.format(Lang.getMsg("gui.dialog.character.description"), chara.getName()));
+        builder.buttonLeft(Lang.getMsg("gui.dialog.character.ok"));
+        builder.buttonRight(Lang.getMsg("gui.dialog.character.cancel"));
         builder.dialogId(charId.getAsInteger());
+        builder.lookAt(lookAt);
         builder.maxLength(255);
         builder.initalText((currentCustomName == null) ? "" : currentCustomName);
-        builder.style("illarion-dialog-input-single");
+
+        builder.style("illarion-dialog-character");
+        log.debug("Built Character dialog: " + builder.toString());
         builders.add(new BuildWrapper(builder, parentArea, createdElement -> {
-            DialogInput control = createdElement.getNiftyControl(DialogInput.class);
+            DialogCharacterControl control = createdElement.getNiftyControl(DialogCharacterControl.class);
             if (control != null) {
                 control.setFocus();
             }
@@ -824,6 +831,8 @@ public final class DialogHandler
         lastMerchantTooltipItem = null;
     }
 
+
+
     @NiftyEventSubscriber(pattern = "inputDialog[0-9]+")
     public void handleInputConfirmedEvent(String topic, @Nonnull DialogInputConfirmedEvent event) {
         if (event.getPressedButton() == DialogButton.LeftButton) {
@@ -833,7 +842,7 @@ public final class DialogHandler
         }
     }
 
-    @NiftyEventSubscriber(pattern = "namingDialog[-0-9]+")
+    @NiftyEventSubscriber(pattern = "characterDialog[-0-9]+")
     public void handleNamingConfirmedEvent(String topic, @Nonnull DialogInputConfirmedEvent event) {
         if (event.getPressedButton() == DialogButton.LeftButton) {
             CharacterId id = new CharacterId(event.getDialogId());

illaclient/src/main/java/illarion/client/net/client/LookAtCharCmd.java
@@ -18,6 +18,8 @@ package illarion.client.net.client;
 import illarion.client.net.CommandList;
 import illarion.common.net.NetCommWriter;
 import illarion.common.types.CharacterId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
 
@@ -27,7 +29,9 @@ import javax.annotation.Nonnull;
  * @author Nop
  * @author Martin Karing &lt;nitram@illarion.org&gt;
  */
-public final class LookatCharCmd extends AbstractCommand {
+public final class LookAtCharCmd extends AbstractCommand {
+    @Nonnull
+    private static final Logger log = LoggerFactory.getLogger(LookAtCharCmd.class);
     /**
      * Mode for looking in a polite way at a character. That leads to the point
      * that the character you are looking at gets no message but you get only
@@ -56,32 +60,12 @@ public final class LookatCharCmd extends AbstractCommand {
 
     /**
      * Default constructor for the look at character command.
-     */
-    public LookatCharCmd() {
-        super(CommandList.CMD_LOOKAT_CHAR);
-    }
-
-    /**
-     * Encode the data of this look at character command and put the values into
-     * the buffer.
-     *
-     * @param writer the interface that allows writing data to the network
-     * communication system
-     */
-    @Override
-    public void encode(@Nonnull NetCommWriter writer) {
-        charId.encode(writer);
-        writer.writeByte(mode);
-    }
-
-    /**
-     * Set the target of the look at and the way the look at is done.
-     *
      * @param lookAtCharId the ID of the char we want to look at
      * @param lookAtMode the mode of the look at so the method used to look at
      * the target character
      */
-    public void examine(CharacterId lookAtCharId, int lookAtMode) {
+    public LookAtCharCmd(CharacterId lookAtCharId, int lookAtMode) {
+        super(CommandList.CMD_LOOKAT_CHAR);
         charId = lookAtCharId;
         mode = (byte) lookAtMode;
     }
@@ -95,5 +79,17 @@ public final class LookatCharCmd extends AbstractCommand {
     @Override
     public String toString() {
         return toString(charId + " mode: " + mode);
+    }
+
+    /**
+     * Encode the data of this look at character command and put the values into the buffer.
+     *
+     * @param writer the interface that allows writing data to the network communication system
+     */
+    @Override
+    public void encode(@Nonnull NetCommWriter writer) {
+        charId.encode(writer);
+        writer.writeByte(mode);
+        log.debug("Encoding a look at char message   for {} with the mode {}", charId, mode);
     }
 }

illaclient/src/main/java/illarion/client/net/server/LookAtCharMsg.java
@@ -15,9 +15,14 @@
  */
 package illarion.client.net.server;
 
+import de.lessvoid.nifty.Nifty;
+import illarion.client.gui.ChatGui;
+import illarion.client.gui.controller.game.DialogHandler;
 import illarion.client.net.CommandList;
 import illarion.client.net.annotations.ReplyMessage;
+import illarion.client.world.World;
 import illarion.common.net.NetCommReader;
+import illarion.common.types.CharacterId;
 import org.jetbrains.annotations.Contract;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -42,7 +47,7 @@ public final class LookAtCharMsg implements ServerReply {
     /**
      * The ID of the character the look at text is related to.
      */
-    private long charId;
+    private CharacterId charId;
 
     /**
      * The text that is the look at result.
@@ -52,14 +57,23 @@ public final class LookAtCharMsg implements ServerReply {
 
     @Override
     public void decode(@Nonnull NetCommReader reader) throws IOException {
-        charId = reader.readUInt();
+        charId = new CharacterId(reader);
         text = reader.readString();
     }
 
     @Nonnull
     @Override
     public ServerReplyResult execute() {
-        log.warn("Received a look at char message for some reason for {} with the text {}", charId, text);
+        log.warn("Executing a look at char message for {} with the text {}", charId, text);
+
+        if (!World.getGameGui().isReady()) {
+            return ServerReplyResult.Reschedule;
+        }
+        if (text.isEmpty()){
+            text = "No description received from the server.";
+        }
+        World.getUpdateTaskManager().addTaskForLater(
+                (container1, delta1) -> World.getGameGui().getDialogInputGui().showCharacterDialog(charId, text));
         return ServerReplyResult.Success;
     }
 

illaclient/src/main/resources/messages_de.properties
@@ -122,10 +122,10 @@ editor.intro.shout=Schreien:
 editor.intro.emote=Handlen:
 editor.intro.ooc=OOC:
 
-gui.dialog.naming.title=Charakter benennen
-gui.dialog.naming.description=Wie soll %1$s benannt werden?
-gui.dialog.naming.ok=OK
-gui.dialog.naming.cancel=Abbrechen
+gui.dialog.character.title=Charakter
+gui.dialog.character.description=Wie soll %1$s benannt werden?
+gui.dialog.character.ok=OK
+gui.dialog.character.cancel=Abbrechen
 
 option.mode=Vollbildmodus
 option.textlog=Logdatei

illaclient/src/main/resources/messages_en.properties
@@ -166,10 +166,10 @@ editor.intro.shout=Shout:
 editor.intro.emote=Act:
 editor.intro.ooc=OOC:
 
-gui.dialog.naming.title=Name character
-gui.dialog.naming.description=How is %1$s called?
-gui.dialog.naming.ok=OK
-gui.dialog.naming.cancel=Cancel
+gui.dialog.character.title=Character
+gui.dialog.character.description=How is %1$s called?
+gui.dialog.character.ok=OK
+gui.dialog.character.cancel=Cancel
 
 option.mode=Fullscreen
 option.textlog=Log file

nifty-controls-illarion/src/main/java/org/illarion/nifty/controls/dialog/input/DialogCharacterControl.java
@@ -1,0 +1,255 @@
+/*
+ * This file is part of the Illarion project.
+ *
+ * Copyright © 2015 - Illarion e.V.
+ *
+ * Illarion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Illarion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package org.illarion.nifty.controls.dialog.input;
+
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.controls.*;
+import de.lessvoid.nifty.controls.window.WindowControl;
+import de.lessvoid.nifty.elements.Element;
+import de.lessvoid.nifty.input.NiftyStandardInputEvent;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.tools.SizeValue;
+import org.bushe.swing.event.EventTopicSubscriber;
+import org.illarion.nifty.controls.DialogInput;
+import org.illarion.nifty.controls.DialogInputConfirmedEvent;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * This is the main control class for Character View window
+ *
+ */
+@Deprecated
+public class DialogCharacterControl extends WindowControl implements DialogInput, EventTopicSubscriber<ButtonClickedEvent> {
+    /**
+     * The instance of the Nifty-GUI that is parent to this control.
+     */
+    @Nullable
+    private Nifty niftyInstance;
+
+    /**
+     * The screen that displays this control.
+     */
+    @Nullable
+    private Screen currentScreen;
+
+    /**
+     * The ID of this dialog.
+     */
+    private int dialogId;
+
+    /**
+     * Helper variable to prevent double firing close events.
+     */
+    private boolean alreadyClosed;
+
+    /**
+     * The label of the left button that is displayed in this dialog.
+     */
+    @Nullable
+    private String buttonLabelLeft;
+
+    /**
+     * The label of the right button that is displayed in this dialog.
+     */
+    @Nullable
+    private String buttonLabelRight;
+
+    /**
+     * The maximal amount of characters allowed to be typed into the input dialog.
+     */
+    private int maxLength;
+
+    /**
+     * The text that is displayed as description in this dialog.
+     */
+    @Nullable
+    private String description;
+
+    @Nonnull
+    private String initialText;
+
+    @Nullable
+    private String lookAt;
+
+    @Override
+    public void bind(
+            @Nonnull Nifty nifty, @Nonnull Screen screen, @Nonnull Element element, @Nonnull Parameters parameter) {
+        super.bind(nifty, screen, element, parameter);
+        niftyInstance = nifty;
+        currentScreen = screen;
+
+        dialogId = Integer.parseInt(parameter.get("dialogId"));
+
+        buttonLabelLeft = parameter.get("buttonLeft");
+        buttonLabelRight = parameter.get("buttonRight");
+        maxLength = parameter.getAsInteger("maxLength", 65535);
+
+        description = parameter.getWithDefault("description", "");
+        lookAt = parameter.getWithDefault("lookAt", "");
+        initialText = parameter.getWithDefault("initialText", "");
+
+        alreadyClosed = false;
+    }
+
+    @Override
+    public void onStartScreen() {
+        assert buttonLabelLeft != null : "Control was not bound correctly";
+        assert buttonLabelRight != null : "Control was not bound correctly";
+        assert description != null : "Control was not bound correctly";
+        assert lookAt != null : "Control was not bound correctly";
+
+        setButtonLabel(DialogButton.LeftButton, buttonLabelLeft);
+        setButtonLabel(DialogButton.RightButton, buttonLabelRight);
+        setDescription(description);
+        setLookAt(lookAt);
+        setMaximalLength(maxLength);
+        setInputText(initialText);
+
+        super.onStartScreen();
+
+        Element element = getElement();
+        Element parent = element.getParent();
+
+        int x = (parent.getWidth() - element.getWidth()) / 2;
+        int y = (parent.getHeight() - element.getHeight()) / 2;
+
+        element.setConstraintX(SizeValue.px(x));
+        element.setConstraintY(SizeValue.px(y));
+
+        parent.layoutElements();
+
+        element.addInputHandler(inputEvent -> {
+            if (inputEvent instanceof NiftyStandardInputEvent) {
+                switch ((NiftyStandardInputEvent) inputEvent) {
+                    case SubmitText:
+                        fireResponse(DialogButton.LeftButton);
+                        return true;
+                    case Escape:
+                        fireResponse(DialogButton.RightButton);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+
+            return false;
+        });
+    }
+
+    private void setLookAt(String text) {
+        Label label = getContent().findNiftyControl("#lookAt", Label.class);
+        if (label == null) {
+            throw new IllegalArgumentException("Failed to fetch description label.");
+        }
+        label.setText(text);
+    }
+
+    @Override
+    public void setButtonLabel(@Nonnull DialogButton button, @Nonnull String label) {
+        Button buttonControl = null;
+        switch (button) {
+            case LeftButton:
+                buttonControl = getContent().findNiftyControl("#buttonLeft", Button.class);
+                break;
+            case RightButton:
+                buttonControl = getContent().findNiftyControl("#buttonRight", Button.class);
+                break;
+        }
+
+        if (buttonControl == null) {
+            throw new IllegalStateException("Failure while fetching button.");
+        }
+
+        buttonControl.setText(label);
+        assert niftyInstance != null : "Control was not bound correctly.";
+        niftyInstance.subscribe(currentScreen, buttonControl.getId(), ButtonClickedEvent.class, this);
+    }
+
+    @Override
+    public void setMaximalLength(int length) {
+        getTextField().setMaxLength(length);
+    }
+
+    @Override
+    public void setDescription(@Nonnull String text) {
+        Label label = getContent().findNiftyControl("#description", Label.class);
+        if (label == null) {
+            throw new IllegalArgumentException("Failed to fetch description label.");
+        }
+        label.setText(text);
+    }
+
+    @Override
+    public void setFocus() {
+        bringToFront();
+        Element textField = getTextField().getElement();
+        if (textField == null) {
+            throw new IllegalStateException("Element of the text field is null. WTF?!");
+        }
+        textField.setFocus();
+    }
+
+    @Override
+    public void onEvent(@Nonnull String topic, ButtonClickedEvent data) {
+        if (topic.contains("#buttonLeft")) {
+            fireResponse(DialogButton.LeftButton);
+        } else {
+            fireResponse(DialogButton.RightButton);
+        }
+    }
+
+    private void fireResponse(@Nonnull DialogButton button) {
+        assert niftyInstance != null : "Control was not bound correctly.";
+        if (alreadyClosed) {
+            return;
+        }
+
+        niftyInstance.publishEvent(getId(), new DialogInputConfirmedEvent(dialogId, button, getInputText()));
+        closeWindow();
+    }
+
+    @Override
+    public void closeWindow() {
+        getElement().hide(() -> getElement().markForRemoval());
+        alreadyClosed = true;
+    }
+
+    @Nonnull
+    String getInputText() {
+        return getTextField().getRealText();
+    }
+
+    private void setInputText(@Nonnull CharSequence text) {
+        getTextField().setText(text);
+    }
+
+    @Nonnull
+    private TextField getTextField() {
+        Element content = getContent();
+        if (content == null) {
+            throw new IllegalStateException("Control doesn't seem to be bound properly. Content is null.");
+        }
+
+        TextField inputArea = content.findNiftyControl("#input", TextField.class);
+        if (inputArea == null) {
+            throw new IllegalStateException("Control is not bound correctly or the underlying object is faulty. Input" +
+                                                    " area not available.");
+        }
+        return inputArea;
+    }
+}

commit 4b79b6866339625a0bbbf40c37ba276c13d8b4f3

Fixed merging error introduced in 23e94eb

illaclient/src/main/java/illarion/client/graphics/Avatar.java
@@ -126,8 +126,6 @@ public final class Avatar extends AbstractEntity<AvatarTemplate> implements Reso
 
     private int showHighlight;
 
-    private Input input;
-
     private Avatar(@Nonnull AvatarTemplate template, @Nonnull Char parentChar) {
         super(template);
         attackMark = new AvatarMarker(MiscImageFactory.ATTACK_MARKER, this);
@@ -308,12 +306,6 @@ public final class Avatar extends AbstractEntity<AvatarTemplate> implements Reso
         }
 
         return super.isEventProcessed(container, delta, event);
-    }
-
-    @Override
-    public void update(@Nonnull GameContainer container, int delta) {
-        input = container.getEngine().getInput();
-        super.update(container, delta);
     }
 
     /**