59e6bee8f891ad245a6892b71498fa70c90b38a5
chelseakay <chelsea.j.kay@gmail.com>
2020-01-25 01:08:57 -0500

    Revert "Added licence notice to newly added files"
    This reverts commit ad9f3d05

--- illaclient/src/main/java/illarion/client/graphics/FrameAnimationMode.java
+++ illaclient/src/main/java/illarion/client/graphics/FrameAnimationMode.java
@@ -1,36 +1,15 @@
-/*
- * This file is part of the Illarion project.
- *
- * Copyright © 2015 - Illarion e.V.
- *
- * Illarion is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Illarion is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
-package illarion.client.graphics;
+package illarion.client.world.movement;
 
-/**
- * @author Martin Karing &lt;nitram@illarion.org&gt;
- */
-public enum FrameAnimationMode {
-    /**
-     * Run animation backwards.
-     */
-    Backwards,
+import illarion.common.types.ServerCoordinate;
 
-    /**
-     * End animation with same frame as it started.
-     */
-    Cyclic,
+import javax.annotation.Nonnull;
 
+public interface TargetTurnHandler extends MovementHandler {
     /**
-     * Keep running in an endless loop.
+     * Turn towards a location.
+     *
+     * @param target the target location
      */
-    Looped
+    void turnTo(@Nonnull ServerCoordinate target);
+
 }
